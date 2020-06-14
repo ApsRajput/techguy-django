2723edf1001d25072e96bab270301633b40cfbaa
apsrajput <apsrajput008@gmail.com>
2020-06-14 12:47:11 +0530

    added product operations

--- techguy/forms.py
+++ techguy/forms.py
@@ -16,3 +16,15 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = "__all__"
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = "__all__"
