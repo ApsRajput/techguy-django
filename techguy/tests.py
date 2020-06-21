ef2b7fc6be5447edc9b3fe5114336f070feeebe4
apsrajput <apsrajput008@gmail.com>
2020-06-21 19:44:34 +0530

    writing tests

--- techguy/tests.py
+++ techguy/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestModels(TestCase):
+    def customer_has_a_product_order(self):
+        order = Order.objects.create(order_id="", status="Delivered")
+        customer = Customer.objects.create(name="Aps Rajput")
+        product = Customer.objects.create(name="product 1", sku="12dq", quantity="2", product_type="Downloadable")
+        book.authors.set([philip.pk, juliana.pk])
+        self.assertEqual(book.authors.count(), 2)
