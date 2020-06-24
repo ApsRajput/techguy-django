e2305d050da245d750ca76554e469246cc533696
apsrajput <apsrajput008@gmail.com>
2020-06-24 18:27:38 +0530

    shifted the database to postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,10 +81,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbname',
+        'USER': 'user',
+        'PASSWORD': 'userpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
